d28334347cc0139cb29b28eb788a5ff60c332a3c
Mirian_Khajishvili <mirian.khajishvili@gmail.comm>
2022-12-20 15:58:23 +0400

    create cards

---
+++ ecommerce-last/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainLayoutModule } from './features/main-layout/main-layout.module';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+ 
+ 
+   
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MainLayoutModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ecommerce-last/src/app/features/main-layout/main-layout.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainLayoutComponent } from './main-layout.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    MainLayoutComponent,
+    HeaderComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+
+  ],
+
+  exports: [
+    MainLayoutComponent
+  ]
+})
+export class MainLayoutModule { }
