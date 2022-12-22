87ea0748ec77c9ff6ba045f5e19e46614d2bc7a2
Mirian_Khajishvili <mirian.khajishvili@gmail.comm>
2022-12-22 12:23:46 +0400

    add photo

--- ecommerce-last/src/app/pages/home/home.component.ts
+++ ecommerce-last/src/app/pages/home/home.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { Product } from 'src/app/core/interfaces';
+import { ProductsService } from 'src/app/core/products.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  products: Product[] = []
+
+
+  sub$ = new Subject()
+
+
+  constructor(
+    private productsService: ProductsService
+  ) { }
 
   ngOnInit(): void {
+    this.getProducts()
+  }
+
+  getProducts(){
+
+    this.productsService.getProducts({})
+    .pipe(takeUntil(this.sub$))
+    .subscribe((products) => {
+      this.products = products
+    })
+  }
+  ngOnDestroy(): void {
+    this.sub$.next(null)
+    this.sub$.complete()
   }
 
 }
